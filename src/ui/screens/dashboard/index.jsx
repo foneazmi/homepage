71215ddd881a5c6de001e0d20a51672ee61724b0
Farkhan Azmi <fliazmi@gmail.com>
2023-01-08 00:04:55 +0700

    disable scroll and misc change

--- src/ui/screens/dashboard/index.jsx
+++ src/ui/screens/dashboard/index.jsx
@@ -31,15 +31,15 @@ export const DashboardScreen = () => {
       <h1 className="text-5xl text-white">
         <Clock />
       </h1>
-      <div className="flex flex-row justify-center p-4 columns-auto">
+      <div className="flex flex-row justify-center mt-6 columns-auto">
         {HOMEPAGE.map((home, index) => (
-          <div className="bg-[#272727] w-40 mx-1 p-3" key={`${index}-homepage`}>
-            <h2 className="text-white text-lg mb-1">{home.title}</h2>
+          <div className="bg-[#272727] w-40 mx-2 p-3" key={`${index}-homepage`}>
+            <h2 className="text-white text-lg mb-2">{home.title}</h2>
             <div className="flex flex-col overflow-auto h-24">
               {home.contents.map((content, index) => (
                 <a
                   key={`${index}-home-content`}
-                  className="text-white text-base opacity-70 hover:opacity-100"
+                  className="text-white py-1 text-base opacity-70 hover:opacity-100"
                   href={content.url}
                 >
                   {content.name}
